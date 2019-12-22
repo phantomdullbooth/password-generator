c36aa519254edbbae9f6f753fb7ceffe3c62f907
Addison <49768250+winslowdev@users.noreply.github.com>
2019-12-21 23:08:24 -0500

    shuffled using F-Y

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 $(() => {
 
-      const $password = $('<h3>')
+      // Fisher-Yates shuffle
+      const randomizeCharacters = (array) => {
 
-      const generatePassword = (length) => {
+            let currentIndex = array.length
+            let temporaryValue;
+            let randomIndex;
+
+            while (0 !== currentIndex) {
+                  randomIndex = Math.floor(Math.random() * currentIndex);
+                  currentIndex -= 1;
+
+                  temporaryValue = array[currentIndex];
+                  array[currentIndex] = array[randomIndex];
+                  array[randomIndex] = temporaryValue;
+            }
+      }
+
+      const generatePassword = (chars) => {
             let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
             let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '?', '/', '<', '>']
             let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
             let password = []
-            let i = 0;
 
-            for (i = 0; i < length; i++)
-                  for (j = 0; j < length; j++)
-                        for (k = 0; k < length; k++) {
-
+            for (i = 0; i < chars; i++)
+                  for (j = 0; j < chars; j++)
+                        for (k = 0; k < chars; k++) {
                               password.push(letters[Math.floor(Math.random() * letters.length)])
                               password.push(symbols[Math.floor(Math.random() * symbols.length)])
                               password.push(numbers[Math.floor(Math.random() * numbers.length)])
                               i++
                               j++
                               k++
-
                         }
-
+            randomizeCharacters(password)
             return password.join('')
       }
 
-      $password.text(generatePassword(6)).appendTo('body')
+      const $password = $('<h3>').text(generatePassword(8)).appendTo('#password')
+
 })
+
+// i like music
+// check out 'Chilly Wonka' by Melodysheep
